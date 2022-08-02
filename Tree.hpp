a1fcd22e9b920b5f843109acd4c3086bd9a2d32b
minsikim-42 <spalstlr321@naver.com>
2022-08-02 14:52:21 +0900

    tree change

--- Tree.hpp
+++ Tree.hpp
@@ -2,55 +2,27 @@
 # define TREE_HPP
 
 #include <iostream>
-#include "Iterator.hpp"
 
 namespace ft
 {
 	template <typename T>
 	struct tree_node
 	{
-		typedef T			node_value_type;
-		typedef T*			node_value_type_pointer;
-		typedef tree_node	node;
-		typedef node*		node_pointer;
+		T			data;
+		tree_node	*parent;
+		tree_node	*right;
+		tree_node	*left;
 
-		node_value_type		value;
-		node_pointer		parent;
-		node_pointer		right;
-		node_pointer		left;
-
-		tree_node() : parent(NULL), right(NULL), left(NULL) {}
+		tree_node() : parent(nullptr), right(nullptr), left(nullptr) {}
 		~tree_node() {}
 
-		void set_parent(node_pointer p) { parent = p; }
-		void set_right(node_pointer p) { right = p; }
-		void set_left(node_pointer p) { left = p; }
+		void set_parent(tree_node p) { parent = p; }
+		void set_right(tree_node p) { right = p; }
+		void set_left(tree_node p) { left = p; }
 		void set_value(const node_value_type &val) { value.second = val.second; } // T가 뭔줄알고 .second를 쓰지
 	};
 
 	// AVL tree
-	template <
-				typename T,
-				typename Compare,
-				typename Node = ft::tree_node<T>,
-				typename Alloc = std::allocator<Node>
-	>
-	class AVL
-	{
-	public:
-		typedef T							value_type;
-		typedef Compare						value_compare;
-		typedef std::ptrdiff_t				difference_type;
-		typedef typename Node::node_pointer	node_pointer;
-		typedef typename ft::tree_iterator<value_type, Node>	iterator;
-	
-	private:
-		node_pointer	_root;
-		node_pointer	_super_root;
-		// allocator_type	_alloc;
-		size_t			_size;
-		value_compare	_value_comp;
-	};
 };
 
 #endif
